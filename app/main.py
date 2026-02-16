12eea625640c5eba5dc348bf2def731de18d24a7
alexevanega <alexevanega@gmail.com>
2026-02-16 11:22:28 -0500

    tested that template mounting with Jinja is working

--- app/main.py
+++ app/main.py
@@ -1,10 +1,24 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 from app.settings import settings
 
 app = FastAPI(title=settings.APP_NAME)
 
 app.mount("/static", StaticFiles(directory="app/static"), name="static")
+
+templates = Jinja2Templates(directory="app/templates")
+
+@app.get("/")
+def home(request: Request):
+    return templates.TemplateResponse(
+        "home.html",
+        {
+            "request": request,
+            "title": settings.APP_NAME,
+            "app_name": settings.APP_NAME
+        }
+    )
 
 @app.get("/api/health")
 def health():
