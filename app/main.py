91b16948e2ea243da58d64479a8cef50ed0cb95e
alexevanega <alexevanega@gmail.com>
2026-02-16 11:12:40 -0500

    testing static mounting

--- app/main.py
+++ app/main.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 from app.settings import settings
 
 app = FastAPI(title=settings.APP_NAME)
+
+app.mount("/static", StaticFiles(directory="app/static"), name="static")
 
 @app.get("/api/health")
 def health():
